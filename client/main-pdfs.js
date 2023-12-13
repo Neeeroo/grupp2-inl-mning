acb61101764ca0a1fea7908f94f673ddadc6bc5f
MalluIndira <priyadarshinireddyindira@gmail.com>
2023-12-13 11:48:05 +0100

    pdfs-html

---
+++ client/main-pdfs.js
@@ -0,0 +1,37 @@
+// Declare a new function named search
+async function search() {
+  // read the user input from the term field in the form searchForm
+  let searchTerm = document.forms.searchForm.term.value;
+  // read the searchType
+  let searchType = document.forms.searchForm.searchType.value;
+  // empty the input field
+  document.forms.searchForm.term.value = '';
+  // read the json
+  let rawData = await fetch('/api/pdfs/' + searchTerm + '/' + searchType);
+  // convert json to a javascript data structure
+  let pdfs = await rawData.json();
+  // create an variable name that initially is an empty string
+  let html = `
+    <p>Du sökte efter "${searchTerm}"...</p>
+    <p>Hittade ${pdfs.length} låtar.</p>
+  `;
+  // loop through the found songs
+  for (let pdf of pdfs) {
+    let meta = pdf.metadata.info;
+    console.log(pdf.fileName);
+    html += `
+      <section>
+        <h2>${meta.Title}</h2>
+        <p><b>Author:</b> ${meta.Author}</p>
+        <p><b>Creator:</b> ${meta.Creator}</p>  
+        <p>
+          <embed src="pdfs/${pdf.fileName}">
+        </p>
+      </section>
+    `;
+  }
+  // Grab the element/tag with the class searchResults
+  let searchResultsElement = document.querySelector('.searchResults');
+  // Change the content of the searchResults element
+  searchResultsElement.innerHTML = html;
+}
